a04e5b6aaceac45b4cd54dd4f46d94000ee28731
Jing Yang <jingyang_carl@qq.com>
2019-02-04 17:54:26 -0800

    Remove Redundant Files

--- Assignment 1 Code/Assignment 1 Code/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Assignment1Code.h"
-#include <QtWidgets/QApplication>
-
-int main(int argc, char *argv[]) {
-	QApplication a(argc, argv);
-	Assignment1Code w;
-	w.show();
-	return a.exec();
-}
