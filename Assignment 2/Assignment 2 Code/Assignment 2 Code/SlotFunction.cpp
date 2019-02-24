a2c17c65390f802ac228c5cdfeeb00833d36e450
Jing Yang <jingyang_carl@qq.com>
2019-02-23 17:02:27 -0800

    Update directories and files

--- Assignment 2/Assignment 2 Code/Assignment 2 Code/SlotFunction.cpp
+++ Assignment 2/Assignment 2 Code/Assignment 2 Code/SlotFunction.cpp
@@ -3,7 +3,7 @@
 void Assignment2Code::PushButtonLoadFile() {
 
 	// Initialization
-	filePath = QFileDialog::getOpenFileName(this, tr("Please Select Image"), "./", tr("RGB File(*.rgb)\n BMP File(*.bmp)"));
+	filePath = QFileDialog::getOpenFileName(this, tr("Please Select Image"), "./Resources", tr("RGB File(*.rgb)\n BMP File(*.bmp)"));
 	rgb = QByteArray();
 	loadStatus = false;
 	FileLoader fileLoader(filePath, rgb, loadStatus);
