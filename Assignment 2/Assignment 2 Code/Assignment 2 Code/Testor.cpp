b673887baa8f90111703c6dd9c40cac85575d9f6
Jing Yang <jingyang_carl@qq.com>
2019-02-26 13:53:00 -0800

    Update Huffman encode test

--- Assignment 2/Assignment 2 Code/Assignment 2 Code/Testor.cpp
+++ Assignment 2/Assignment 2 Code/Assignment 2 Code/Testor.cpp
@@ -1,64 +1,80 @@
 #include "Testor.h"
 #include "qvector.h"
-#include "qbitarray.h"
 #include <queue>
 #include "qmap.h"
-#include "qpair.h"
+#include "qstack.h"
 
 void Testor::run() {
+	QMap<QString, float> input;
+	input.insert("1", 5);
+	input.insert("2", 7);
+	input.insert("3", 10);
+	input.insert("4", 15);
+	input.insert("5", 20);
+	input.insert("6", 45);
 
 	// using min heap to implement haffman code
-
-	struct Compare {
-		bool operator() (QPair<QString, float> node1, QPair<QString, float> node2) {
-			return node1.second > node2.second;
-		}
-	};
 
 	struct BinaryTreeNode {
 		QString content;
 		float frequency;
 		BinaryTreeNode * left;
 		BinaryTreeNode * right;
+
+		BinaryTreeNode(float frequency) {
+			this->content = "";
+			this->frequency = frequency;
+			this->left = this->right = NULL;
+		}
 		BinaryTreeNode(QString content, float frequency) {
 			this->content = content;
 			this->frequency = frequency;
-			this->left = NULL;
-			this->right = NULL;
+			this->left = this->right = NULL;
+		}
+		BinaryTreeNode(const BinaryTreeNode & node) {
+			this->content = node.content;
+			this->frequency = node.frequency;
+			this->left = node.left;
+			this->right = node.right;
+		}
+		constexpr bool operator< (const BinaryTreeNode & node) const {
+			return frequency > node.frequency;
 		}
 	};
 
-	QMap<QString, float> input;
-	input.insert("0,0", 1);
-	input.insert("0,1", 3);
-	input.insert("1,2", 1);
-	input.insert("2,1", 1);
-	input.insert("2", 1);
+	std::priority_queue<BinaryTreeNode> minHeap;
 
+	for (int i = 0; i < input.size(); i++) {
+		QMap<QString, float>::iterator iter = input.begin() + i;
+		BinaryTreeNode node(iter.key(), iter.value());
+		minHeap.push(node);
+	}
 
+	while (minHeap.size() != 1) {
+		BinaryTreeNode node_1(minHeap.top());
+		minHeap.pop();
+		BinaryTreeNode node_2(minHeap.top());
+		minHeap.pop();
+		BinaryTreeNode node(node_1.frequency + node_2.frequency);
+		node.left = new BinaryTreeNode(node_1);
+		node.right = new BinaryTreeNode(node_2);
+		minHeap.push(node);
+	}
 
-	std::priority_queue<QPair<QString, float>, QVector<QPair<QString, float>>, Compare> minHeap;
+	BinaryTreeNode * huffmanTree = new BinaryTreeNode(minHeap.top());
+	QMap<QString, QString> huffmanCode;
 
-	minHeap.push(QPair<QString, int>("10", 10));
-	minHeap.push(QPair<QString, int>("20", 20));
-	minHeap.push(QPair<QString, int>("30", 30));
-	minHeap.push(QPair<QString, int>("5", 5));
-	minHeap.push(QPair<QString, int>("15", 15));
+	struct Encode {
+		static void HuffmanEncode(BinaryTreeNode *huffmanTree, QString code, QMap<QString, QString> & huffmanCode) {
+			if (!huffmanTree->left && !huffmanTree->right) {
+				huffmanCode.insert(huffmanTree->content, code);
+			}
+			else {
+				HuffmanEncode(huffmanTree->left, code + '1', huffmanCode);
+				HuffmanEncode(huffmanTree->right, code + '0', huffmanCode);
+			}
+		};
+	};
 
-	QVector<QPair<QString, int>> result;
-	result.append(minHeap.top());
-	minHeap.pop();
-	result.append(minHeap.top());
-	minHeap.pop();
-	result.append(minHeap.top());
-	minHeap.pop();
-	result.append(minHeap.top());
-	minHeap.pop();
-	result.append(minHeap.top());
-	minHeap.pop();
-
-
-
-
-	result;
+	Encode::HuffmanEncode(huffmanTree, QString(), huffmanCode);
 }
