8ff8a9fca5890a2d8912b78eb772f2a878643741
Jing Yang <jingyang_carl@qq.com>
2019-04-11 00:39:38 -0700

    Update

--- Final Project/Test/Test/main.cpp
+++ Final Project/Test/Test/main.cpp
@@ -1,8 +1,24 @@
 #include <QtCore/QCoreApplication>
+#include <qaudiobuffer.h>
+#include <qfile.h>
+#include "wavfile.h"
+#include <qdebug.h>
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
+
+	WavFile wavFile;
+	QString filePath = "D:/576 Data/dataset/Videos/data_test1.wav";
+	wavFile.setFileName(filePath);
+
+	if (wavFile.exists() == false)
+		return a.exec();
+	wavFile.open(filePath);
+
+	QAudioBuffer wavBuffer(wavFile.readAll(), wavFile.fileFormat());
+
+	const quint16 *data = wavBuffer.constData<quint16>();
 
 	return a.exec();
 }
