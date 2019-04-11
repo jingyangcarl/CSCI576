87e25c4e46836d6a253beb4605e20480c01235c3
Jing Yang <jingyang_carl@qq.com>
2019-04-11 14:05:45 -0700

    cut audio

--- Final Project/Test/Test/main.cpp
+++ Final Project/Test/Test/main.cpp
@@ -1,26 +1,28 @@
 #include <QtCore/QCoreApplication>
 #include <qaudiobuffer.h>
+#include <qdebug.h>
 #include <qfile.h>
 #include "wavfile.h"
-#include <qdebug.h>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
 	QCoreApplication a(argc, argv);
 
+	// read WAV file
 	WavFile wavFile;
-	QString filePath = "D:/576 Data/dataset/Videos/data_test1.wav";
+	QString filePath = "D:/576 Data/dataset2/Videos/data_test2.wav";
 	wavFile.setFileName(filePath);
 
+	// open WAV file
 	if (wavFile.exists() == false)
 		return a.exec();
 	wavFile.open(filePath);
 
+	// Read samples
 	QAudioBuffer wavBuffer(wavFile.readAll(), wavFile.fileFormat());
-
 	const qint16 *data = wavBuffer.constData<qint16>();
-	//const short *data = wavBuffer.constData<short>();
-	//const float *data = wavBuffer.constData<float>();
 
+	// test on buffer
 	// "data_test1.wav" has 14394335 samples
 	qDebug() << "QAudioBuffer Byte Count:" << wavBuffer.byteCount();
 	// output: 28788670
@@ -29,8 +31,25 @@ int main(int argc, char *argv[]) {
 	qDebug() << "QAudioBuffer Frame Count:" << wavBuffer.frameCount();
 	// output: 14394335
 
-	for (int i = 0; i < 10; i++)
-		qDebug() << (double)data[927 + i];
+	// cut audio
+	int winLen = 200;
+	QVector<int> subAudiosIndex;
+	int lastCut = 0;
+	for (int i = 0; i < wavBuffer.sampleCount() / winLen; i++) {
+		int quantiSum = 0;
+		for (int j = 0; j < winLen; j++)
+			quantiSum += abs(data[i*winLen + j]);
+		if (quantiSum / winLen < 20) {
+			subAudiosIndex.append(i);
+		}
+	}
+
+	QVector<int>::iterator iter;
+	if(subAudiosIndex.size() <= 1)
+		return a.exec();
+	for (iter = subAudiosIndex.begin(); iter != subAudiosIndex.end(); iter++) {
+		std::cout << (*iter) << '\t' << (*iter) / (48000.0 / winLen) << std::endl;
+	}
 
 	return a.exec();
 }
